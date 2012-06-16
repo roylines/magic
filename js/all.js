0d90006c6103985a6453c05c237d42b9a6ba2695
Roy Lines <roy@roylines.co.uk>
2012-06-16 09:51:50 +0100

    finalised simple add generation

--- js/all.js
+++ js/all.js
@@ -7,16 +7,16 @@
 
 $(document).ready(function() {
   var randomAds = $.randomize(ads);
-  jQuery.each(randomAds, function() {
-  $("#ads").append(
-    '\
-    <li class="span3">\
-    <div class="thumbnail">\
-    <h5>' + this.t + '</h5>\
-    <img width="115" height="140" src="' + this.g + '" alt="">\
-    <p>Price: £' + this.p + ' <a href="http://rover.ebay.com/rover/1/710-53481-19255-0/1?icep_ff3=2&toolid=10005&campid=5335890464&customid=&icep_item=' + this.i + '&ipn=psmain&icep_vectorid=229508&kwid=902099&mtid=824&kw=rss">Bid Now</a></p>\
-    </div>\
-    </li>');
-  });
+  jQuery.each(randomAds, function(i) {
+  if(i < 3) {
+    $('#ephemera').append(
+    '<div>\
+      <a href="http://rover.ebay.com/rover/1/710-53481-19255-0/1?icep_ff3=2&toolid=10005&campid=5335890464&customid=&icep_item=' + this.i + '&ipn=psmain&icep_vectorid=229508&kwid=902099&mtid=824&kw=rss">\
+      <h4>' + this.t + '  <small>(£' + parseFloat(this.p).toFixed(2) + ')</small></h4>\
+      <img src="' + this.g + '" alt="' + this.t + '" title="' + this.t + '">\
+     </div>'
+    );
+  }
  });
+});
 
